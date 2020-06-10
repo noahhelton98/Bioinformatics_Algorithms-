9ed6d98b9d8079e71e701d3eb90f5b19c2d3c4f2
Noah Helton <64622103+noahhelton98@users.noreply.github.com>
2020-06-09 23:14:18 -0400

    Update Chapter 1.py

--- Chapter 1.py
+++ Chapter 1.py
@@ -33,6 +33,24 @@ def Reverse_complement(dna):
             complement += 'C'
     return complement 
 
+#nucleotide Count 
 
+def counts(dna):
+    count_A = dna.count('A')
+    count_T = dna.count('T')
+    count_G = dna.count('G')
+    count_C = dna.count('C')
 
-#ATCG Count 
+    return count_A, count_C, count_G, count_T
+                 
+                 
+#HammingCount 
+
+                 
+def Hamming_distance(str1, str2):
+    count = 0
+    if len(str1) == len(str2)
+        for i in range(0, len(str1)):
+            if str1[i] != str2[i]:
+                count += 1
+    return count
